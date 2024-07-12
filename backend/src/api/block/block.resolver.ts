6ca9aff19d64ee1752f58c43a1ff613c7b43b7d4
ahonn <yuexunjiang@gmail.com>
2024-07-12 23:04:02 +1000

    feat: add prisma module and impl fake blockByHash query

--- backend/src/api/block/block.resolver.ts
+++ backend/src/api/block/block.resolver.ts
@@ -1,12 +1,17 @@
 import { Args, Query, Resolver } from '@nestjs/graphql';
 import { Block } from './models/block.model';
+import { BlockService } from './block.service';
 
 @Resolver(() => Block)
 export class BlockResolver {
-  @Query(() => Block)
-  public async block(@Args('height') height: string): Promise<Block> {
-    return {
-      height,
-    };
+  constructor(private blockService: BlockService) {}
+
+  @Query(() => Block, { nullable: true })
+  public async blockByHash(@Args('hash') hash: string): Promise<Block> {
+    const block = await this.blockService.getBlockByHash(hash);
+    if (!block) {
+      return null;
+    }
+    return Block.from(block);
   }
 }
--- backend/src/api/block/models/block.model.ts
+++ backend/src/api/block/models/block.model.ts
@@ -1,7 +1,14 @@
 import { Field, Int, ObjectType } from '@nestjs/graphql';
+import PrismaClient from '@prisma/client';
 
 @ObjectType({ description: 'block' })
 export class Block {
   @Field(() => Int)
-  height: string;
+  hash: string;
+
+  public static from(block: PrismaClient.Block): Block {
+    return {
+      hash: block.blockHash,
+    };
+  }
 }
