9bc487b52ac00d8e2b8382fbf8d685c26aede4c3
ahonn <yuexunjiang@gmail.com>
2024-07-18 20:55:17 +1000

    feat: add bitcoin/transaction vin & vout

--- backend/src/modules/bitcoin/transaction/transaction.model.ts
+++ backend/src/modules/bitcoin/transaction/transaction.model.ts
@@ -1,6 +1,70 @@
 import { Field, Float, Int, ObjectType } from '@nestjs/graphql';
 import * as BitcoinApi from 'src/core/bitcoin-api/bitcoin-api.schema';
 
+@ObjectType({ description: 'Bitcoin Output' })
+export class BitcoinOutput {
+  @Field(() => String)
+  scriptpubkey: string;
+
+  @Field(() => String)
+  scriptpubkeyAsm: string;
+
+  @Field(() => String)
+  scriptpubkeyType: string;
+
+  @Field(() => String, { nullable: true })
+  scriptpubkeyAddress: string;
+
+  @Field(() => Float)
+  value: number;
+
+  public static from(output: BitcoinApi.Output) {
+    return {
+      scriptpubkey: output.scriptpubkey,
+      scriptpubkeyAsm: output.scriptpubkey_asm,
+      scriptpubkeyType: output.scriptpubkey_type,
+      scriptpubkeyAddress: output.scriptpubkey_address,
+      value: output.value,
+    };
+  }
+}
+
+@ObjectType({ description: 'Bitcoin Input' })
+export class BitcoinInput {
+  @Field(() => String)
+  txid: string;
+
+  @Field(() => Int)
+  vout: number;
+
+  @Field(() => BitcoinOutput, { nullable: true })
+  prevout: BitcoinOutput;
+
+  @Field(() => String)
+  scriptsig: string;
+
+  @Field(() => String)
+  scriptsigAsm: string;
+
+  @Field(() => Boolean)
+  isCoinbase: boolean;
+
+  @Field(() => Int)
+  sequence: number;
+
+  public static from(input: BitcoinApi.Input) {
+    return {
+      txid: input.txid,
+      vout: input.vout,
+      prevout: input.prevout ? BitcoinOutput.from(input.prevout) : null,
+      scriptsig: input.scriptsig,
+      scriptsigAsm: input.scriptsig_asm,
+      isCoinbase: input.is_coinbase,
+      sequence: input.sequence,
+    };
+  }
+}
+
 export type BitcoinBaseTransaction = BitcoinTransaction;
 
 @ObjectType({ description: 'Bitcoin Transaction' })
@@ -16,6 +80,12 @@ export class BitcoinTransaction {
 
   @Field(() => Int)
   version: number;
+
+  @Field(() => [BitcoinInput])
+  vin: BitcoinInput[];
+
+  @Field(() => [BitcoinOutput])
+  vout: BitcoinOutput[];
 
   @Field(() => Float)
   size: number;
@@ -42,6 +112,8 @@ export class BitcoinTransaction {
       blockHash: tx.status.block_hash,
       txid: tx.txid,
       version: tx.version,
+      vin: tx.vin.map(BitcoinInput.from),
+      vout: tx.vout.map(BitcoinOutput.from),
       size: tx.size,
       locktime: new Date(tx.locktime),
       weight: tx.weight,
