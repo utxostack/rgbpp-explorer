e51cd0b8f1b2e19b1111c16ff911787fb474b097
ahonn <yuexunjiang@gmail.com>
2024-07-30 18:50:15 +1000

    fix: cleanup bitcoin base dataloader

--- backend/src/modules/bitcoin/block/dataloader/base.ts
+++ backend/src/modules/bitcoin/block/dataloader/base.ts
@@ -8,54 +8,6 @@ export abstract class BitcoinBaseLoader {
   abstract bitcoinApiService: BitcoinApiService;
   abstract cacheManager: Cache;
 
-  // private async getBlockHashByHeight(height: number): Promise<string> {
-  //   const cacheKey = `bitcoinApiService#getBlockHeight:${height}`;
-  //   const cached = await this.cacheManager.get<string>(cacheKey);
-  //   if (cached) {
-  //     return cached;
-  //   }
-  //   const hash = await this.bitcoinApiService.getBlockHeight({ height });
-  //   await this.cacheManager.set(cacheKey, hash);
-  //   return hash;
-  // }
-
-  // private async getBlockByHash(hash: string): Promise<BitcoinApi.Block> {
-  //   const cacheKey = `bitcoinApiService#getBlock:${hash}`;
-  //   const cached = await this.cacheManager.get<BitcoinApi.Block>(cacheKey);
-  //   if (cached) {
-  //     return cached;
-  //   }
-  //   const block = await this.bitcoinApiService.getBlock({ hash });
-  //   const ttl = Date.now() - block.timestamp * 1000;
-  //   await this.cacheManager.set(cacheKey, block, ttl);
-  //   return block;
-  // }
-
-  // private async getBlockTxsByHash(
-  //   hash: string,
-  //   startIndex: number,
-  // ): Promise<BitcoinApi.Transaction[]> {
-  //   const cacheKey = `bitcoinApiService#getBlockTxs:${hash}:${startIndex}`;
-  //   const cached = await this.cacheManager.get<BitcoinApi.Transaction[]>(cacheKey);
-  //   if (cached) {
-  //     return cached;
-  //   }
-  //   const txs = await this.bitcoinApiService.getBlockTxs({ hash, startIndex });
-  //   await this.cacheManager.set(cacheKey, txs);
-  //   return txs;
-  // }
-
-  // private async getBlockTxidsByHash(hash: string): Promise<string[]> {
-  //   const cacheKey = `bitcoinApiService#getBlockTxids:${hash}`;
-  //   const cached = await this.cacheManager.get<string[]>(cacheKey);
-  //   if (cached) {
-  //     return cached;
-  //   }
-  //   const txids = await this.bitcoinApiService.getBlockTxids({ hash });
-  //   await this.cacheManager.set(cacheKey, txids);
-  //   return txids;
-  // }
-
   protected async getBlock(hashOrHeight: string): Promise<BitcoinApi.Block> {
     if (hashOrHeight.startsWith('0')) {
       const block = await this.bitcoinApiService.getBlock({ hash: hashOrHeight });
