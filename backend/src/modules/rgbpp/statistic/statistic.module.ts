fc8613543766c7a39ebf8a71ab65dfae90301d61
ahonn <yuexunjiang@gmail.com>
2024-07-30 19:37:22 +1000

    feat: impl rgbpp assets holders resolve field

--- backend/src/modules/rgbpp/statistic/statistic.module.ts
+++ backend/src/modules/rgbpp/statistic/statistic.module.ts
@@ -1,9 +1,27 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 import { CkbExplorerModule } from 'src/core/ckb-explorer/ckb-explorer.module';
 import { RgbppStatisticResolver } from './statistic.resolver';
+import { RgbppStatisticService } from './statistic.service';
+import { CkbRpcModule } from 'src/core/ckb-rpc/ckb-rpc.module';
+import { BitcoinApiModule } from 'src/core/bitcoin-api/bitcoin-api.module';
+import { Cron, CronExpression } from '@nestjs/schedule';
 
 @Module({
-  imports: [CkbExplorerModule],
-  providers: [RgbppStatisticResolver],
+  imports: [CkbExplorerModule, CkbRpcModule, BitcoinApiModule],
+  providers: [RgbppStatisticResolver, RgbppStatisticService],
+  exports: [RgbppStatisticService],
 })
-export class RgbppStatisticModule {}
+export class RgbppStatisticModule {
+  private readonly logger = new Logger(RgbppStatisticModule.name);
+
+  constructor(
+    private rgbppStatisticService: RgbppStatisticService,
+  ) {}
+
+  @Cron(CronExpression.EVERY_10_MINUTES)
+  public async collectRgbppAssetsHoldersCronTask() {
+    this.logger.log('Collecting RGBPP assets holders...');
+    const holders = await this.rgbppStatisticService.collectRgbppAssetsHolders();
+    await this.rgbppStatisticService.setRgbppAssetsHolders(holders);
+  }
+}
