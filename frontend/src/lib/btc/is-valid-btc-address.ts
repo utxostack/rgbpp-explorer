b89079f8ff532142fec7e278b9e7390e6aa9efec
INS <j893412899@outlook.com>
2024-08-05 19:06:51 +0800

    fix(frontend): some issues

--- frontend/src/lib/btc/is-valid-btc-address.ts
+++ frontend/src/lib/btc/is-valid-btc-address.ts
@@ -3,10 +3,9 @@ import * as ecc from 'tiny-secp256k1'
 
 import { env } from '@/constants/env'
 
-bitcoin.initEccLib(ecc)
-
 export function isValidBTCAddress(address: string) {
   try {
+    bitcoin.initEccLib(ecc)
     bitcoin.address.toOutputScript(address, env.public.IS_MAINNET ? bitcoin.networks.bitcoin : bitcoin.networks.testnet)
     return true
   } catch (e) {
