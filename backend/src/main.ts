27def58371b9f67f3a447bc3c4ab306f5cf1c489
mfuuzy <uangian@live.com>
2024-08-05 15:25:10 +0800

    chore: enable Cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,11 @@ import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+  app.enableCors({
+    origin: ['https://explorer.utxostack.network', 'http://explorer.utxostack.network','http://localhost:3000'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    credentials: true,
+  });
   await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
