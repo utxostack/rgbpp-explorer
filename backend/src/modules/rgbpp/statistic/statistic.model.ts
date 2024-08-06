06ed8db4776f6eebe5bc496b5d32655189db6224
ahonn <yuexunjiang@gmail.com>
2024-08-06 23:07:09 +1000

    feat: add rgbpp stats latest 24 hours L1/L2 transaction count field

--- backend/src/modules/rgbpp/statistic/statistic.model.ts
+++ backend/src/modules/rgbpp/statistic/statistic.model.ts
@@ -2,11 +2,17 @@ import { Field, ObjectType } from '@nestjs/graphql';
 
 export type RgbppBaseStatistic = Record<string, never>;
 
-@ObjectType({ description: 'RGB++ Coin' })
+@ObjectType({ description: 'RGB++ Statistic' })
 export class RgbppStatistic {
   @Field(() => Number)
   transactionsCount: number;
 
   @Field(() => Number)
   holdersCount: number;
+
+  @Field(() => Number)
+  latest24HoursL1TransactionsCount: number;
+
+  @Field(() => Number)
+  latest24HoursL2TransactionsCount: number;
 }
--- backend/src/modules/rgbpp/statistic/statistic.module.ts
+++ backend/src/modules/rgbpp/statistic/statistic.module.ts
@@ -1,26 +1,42 @@
-import { forwardRef, Logger, Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { CkbExplorerModule } from 'src/core/ckb-explorer/ckb-explorer.module';
 import { RgbppStatisticResolver } from './statistic.resolver';
 import { RgbppStatisticService } from './statistic.service';
 import { CkbRpcModule } from 'src/core/ckb-rpc/ckb-rpc.module';
 import { BitcoinApiModule } from 'src/core/bitcoin-api/bitcoin-api.module';
-import { Cron, CronExpression } from '@nestjs/schedule';
 import { RgbppModule } from '../rgbpp.module';
+import { CronExpression, SchedulerRegistry } from '@nestjs/schedule';
+import { CronJob } from 'cron';
+import { CkbScriptModule } from 'src/modules/ckb/script/script.module';
 
 @Module({
-  imports: [CkbExplorerModule, CkbRpcModule, BitcoinApiModule, forwardRef(() => RgbppModule)],
+  imports: [
+    CkbRpcModule,
+    CkbExplorerModule,
+    CkbRpcModule,
+    BitcoinApiModule,
+    CkbScriptModule,
+    forwardRef(() => RgbppModule),
+  ],
   providers: [RgbppStatisticResolver, RgbppStatisticService],
   exports: [RgbppStatisticService],
 })
 export class RgbppStatisticModule {
-  private readonly logger = new Logger(RgbppStatisticModule.name);
-
-  constructor(private rgbppStatisticService: RgbppStatisticService) {}
-
-  @Cron(CronExpression.EVERY_10_MINUTES)
-  public async collectRgbppAssetsHoldersCronTask() {
-    this.logger.log('Collecting RGBPP assets holders...');
-    const holders = await this.rgbppStatisticService.collectRgbppAssetsHolders();
-    await this.rgbppStatisticService.setRgbppAssetsHolders(holders);
+  constructor(
+    private rgbppStatisticService: RgbppStatisticService,
+    private schedulerRegistry: SchedulerRegistry,
+  ) {
+    this.schedulerRegistry.addCronJob(
+      'collectLatest24L1Transactions',
+      new CronJob(CronExpression.EVERY_HOUR, () => {
+        this.rgbppStatisticService.collectLatest24L1Transactions();
+      }),
+    );
+    this.schedulerRegistry.addCronJob(
+      'collectLatest24L2Transactions',
+      new CronJob(CronExpression.EVERY_HOUR, () => {
+        this.rgbppStatisticService.collectLatest24L2Transactions();
+      }),
+    );
   }
 }
