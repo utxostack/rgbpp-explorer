e9a09a9930d337402d99a6ef162aa88db11e959a
INS <j893412899@outlook.com>
2024-08-06 14:10:57 +0800

    fix(frontend): leap direction

--- frontend/src/lib/resolve-layer-type-from-rgbpp-transaction.ts
+++ frontend/src/lib/resolve-layer-type-from-rgbpp-transaction.ts
@@ -9,12 +9,8 @@ export function resolveLayerTypeFromRGBppTransaction(
     case LeapDirection.LeapIn:
       return 'l1-l2'
     case LeapDirection.Within:
-      if (tx.ckbTransaction) return 'l2'
-      if (tx.btcTransaction) return 'l1'
-      return
+      return 'l1'
     default:
-      if (tx.ckbTransaction) return 'l2'
-      if (tx.btcTransaction) return 'l1'
       return
   }
 }
