474971e5a15e0db54e9376b89a26acf19d28bbe4
INS <j893412899@outlook.com>
2024-08-07 18:05:56 +0800

    feat(frontend): home typing animation

--- frontend/src/components/home-title.tsx
+++ frontend/src/components/home-title.tsx
@@ -1,17 +1,46 @@
 'use client'
 
 import { Trans } from '@lingui/macro'
+import { useEffect, useRef } from 'react'
+import { Box, Flex } from 'styled-system/jsx'
+import Typed from 'typed.js'
 
-import { Heading, Text } from '@/components/ui'
+import { Heading } from '@/components/ui'
 
 export function HomeTitle() {
+  const el = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: [
+        '<span style="color: #F7931A">BTC</span>',
+        '<span style="color: red">R</span><span style="color: green">G</span><span style="color: blue">B</span>++',
+        '<span style="color: var(--colors-brand)">UTXO</span>',
+      ],
+      typeSpeed: 50,
+      loop: true,
+    })
+
+    return () => {
+      typed.destroy()
+    }
+  }, [])
+
   return (
-    <Heading mt="10%" mb="54px" fontSize="62px" fontWeight="semibold">
+    <Heading
+      mt="10%"
+      mb="54px"
+      fontSize="62px"
+      fontWeight="semibold"
+      lineHeight="100px"
+      display="flex"
+      alignItems="center"
+    >
       <Trans>
         Explore the
-        <Text as="span" color="#F7931A" mx="12px">
-          BTC
-        </Text>
+        <Flex display="flex" alignItems="center" w="240px" mx="20px" h="100px" justify="center">
+          <Box ref={el} display="inline-block" h="100px" />
+        </Flex>
         Ecosystem
       </Trans>
     </Heading>
