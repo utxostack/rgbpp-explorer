69188cbb8644360648e29b582c478f2cf1079105
ahonn <yuexunjiang@gmail.com>
2024-08-09 15:36:42 +1000

    fix: remove rest bitcoin base model interface and set bitcoin transaction time nullable

--- backend/src/modules/bitcoin/address/address.model.ts
+++ backend/src/modules/bitcoin/address/address.model.ts
@@ -1,30 +1,11 @@
-import { Field, Float, ObjectType } from '@nestjs/graphql';
-import { BitcoinTransaction } from '../transaction/transaction.model';
-import { RgbppAddress } from 'src/modules/rgbpp/address/address.model';
-
-export type BitcoinBaseAddress = Pick<BitcoinAddress, 'address'>;
+import { Field, ObjectType } from '@nestjs/graphql';
 
 @ObjectType({ description: 'Bitcoin Address' })
 export class BitcoinAddress {
   @Field(() => String)
   address: string;
 
-  @Field(() => Float)
-  satoshi: number;
-
-  @Field(() => Float)
-  pendingSatoshi: number;
-
-  @Field(() => Float, { nullable: true })
-  transactionsCount: number | null;
-
-  @Field(() => [BitcoinTransaction])
-  transactions: BitcoinTransaction[];
-
-  @Field(() => RgbppAddress)
-  rgbppAddress: RgbppAddress;
-
-  public static from(address: string): BitcoinBaseAddress {
+  public static from(address: string): BitcoinAddress {
     return {
       address,
     };
