97f16a0cff3714473789d6a6a6e826fd3acce5f3
ahonn <yuexunjiang@gmail.com>
2024-09-04 20:53:18 +1000

    fix: fix script service di

--- backend/src/modules/ckb/script/dob.service.ts
+++ backend/src/modules/ckb/script/dob.service.ts
@@ -3,11 +3,18 @@ import { Injectable, Logger } from '@nestjs/common';
 import { DOB_TYPESCRIPTS } from 'src/constants';
 import { BaseScriptService } from './base/base-script.service';
 import { CellType } from './script.model';
+import { CkbRpcWebsocketService } from 'src/core/ckb-rpc/ckb-rpc-websocket.service';
+import { Env } from 'src/env';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class DigitalObjectService extends BaseScriptService {
   protected logger = new Logger(DigitalObjectService.name);
   public type: CellType = CellType.DOB;
+
+  constructor(configService: ConfigService<Env>, ckbRpcService: CkbRpcWebsocketService) {
+    super(configService, ckbRpcService);
+  }
 
   public getScripts(): Script[] {
     const networkType = this.configService.get('NETWORK');
--- backend/src/modules/ckb/script/mnft.service.ts
+++ backend/src/modules/ckb/script/mnft.service.ts
@@ -3,11 +3,18 @@ import { Injectable, Logger } from '@nestjs/common';
 import { MNFT_TYPESCRIPTS } from 'src/constants';
 import { BaseScriptService } from './base/base-script.service';
 import { CellType } from './script.model';
+import { ConfigService } from '@nestjs/config';
+import { Env } from 'src/env';
+import { CkbRpcWebsocketService } from 'src/core/ckb-rpc/ckb-rpc-websocket.service';
 
 @Injectable()
 export class MNFTService extends BaseScriptService {
   protected logger = new Logger(MNFTService.name);
   public type: CellType = CellType.MNFT;
+
+  constructor(configService: ConfigService<Env>, ckbRpcService: CkbRpcWebsocketService) {
+    super(configService, ckbRpcService);
+  }
 
   public getScripts(): Script[] {
     const networkType = this.configService.get('NETWORK');
