3fe52b5ce53d4580572d4690a235856062af1fc1
ahonn <yuexunjiang@gmail.com>
2024-09-20 11:22:39 +1000

    fix: optimize caching and health checks (#177)

--- backend/src/middlewares/field-performance.middleware.ts
+++ backend/src/middlewares/field-performance.middleware.ts
@@ -6,23 +6,20 @@ export const fieldPerformanceMiddleware: FieldMiddleware = async (
   next: NextFn,
 ) => {
   const now = performance.now();
-  const memoryUsage = process.memoryUsage();
+  const memoryUsageNow = process.memoryUsage();
 
   const value = await next();
 
   const executionTime = performance.now() - now;
-  const memoryUsageAfter = process.memoryUsage();
+  const memoryUsage = process.memoryUsage().heapUsed - memoryUsageNow.heapUsed;
 
   Sentry.setContext('graphql', {
     executionTime,
+    memoryUsage,
     field: ctx.info.fieldName,
     parent: ctx.info.parentType.name,
   });
   Sentry.setMeasurement('graphql.executionTime', executionTime, 'millisecond');
-  Sentry.setMeasurement(
-    'graphql.memoryUsage',
-    memoryUsageAfter.heapUsed - memoryUsage.heapUsed,
-    'byte',
-  );
+  Sentry.setMeasurement('graphql.memoryUsage', memoryUsage, 'byte');
   return value;
 };
