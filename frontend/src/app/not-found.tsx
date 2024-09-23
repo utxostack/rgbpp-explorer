2321c4225ea0e4465bac95c50557358eb99bfd68
INS <j893412899@outlook.com>
2024-09-23 09:40:45 +0800

    fix(frontend): 404 page i18n

--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { t } from '@lingui/macro'
+import { Trans } from '@lingui/macro'
 import { Center } from 'styled-system/jsx'
 
 import NotFoundSVG from '@/assets/not-found.svg'
@@ -11,14 +11,13 @@ export default function NotFound() {
   return (
     <Center flexDir="column" flex={1} bg="bg.card" maxW="content" w="100%" my="30px" p="30px">
       <NotFoundSVG w="200px" h="200px" />
-      <Text
-        fontSize="14px"
-        fontWeight="medium"
-        color="text.third"
-        mb="24px"
-      >{t`Sorry, the page you visited does not exist`}</Text>
+      <Text fontSize="14px" fontWeight="medium" color="text.third" mb="24px">
+        <Trans>Sorry, the page you visited does not exist</Trans>
+      </Text>
       <Link href="/">
-        <Button size="sm">{t`Back to Home`}</Button>
+        <Button size="sm">
+          <Trans>Back to Home</Trans>
+        </Button>
       </Link>
     </Center>
   )
