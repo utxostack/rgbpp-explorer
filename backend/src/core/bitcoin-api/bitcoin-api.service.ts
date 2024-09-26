928e9fb97f2f4769576356bb3531125295d28b2c
ahonn <yuexunjiang@gmail.com>
2024-09-26 21:35:37 +1000

    feat: implement Apollo Server caching and add logging plugin

--- backend/src/core/bitcoin-api/bitcoin-api.service.ts
+++ backend/src/core/bitcoin-api/bitcoin-api.service.ts
@@ -10,7 +10,6 @@ import { ChainInfo, Transaction } from './bitcoin-api.schema';
 import { ONE_HOUR_MS, ONE_MONTH_MS, TEN_MINUTES_MS } from 'src/common/date';
 import { Cacheable } from 'src/decorators/cacheable.decorator';
 import * as Sentry from '@sentry/nestjs';
-import { PLimit } from 'src/decorators/plimit.decorator';
 
 type MethodParameters<T, K extends keyof T> = T[K] extends (...args: infer P) => any ? P : never;
 type MethodReturnType<T, K extends keyof T> = T[K] extends (...args: any[]) => infer R ? R : never;
@@ -180,7 +179,6 @@ export class BitcoinApiService {
     key: ({ address }) => `getAddress:${address}`,
     ttl: 10_000,
   })
-  @PLimit({ concurrency: 100 })
   public async getAddress({ address }: { address: string }) {
     return this.call('getAddress', { address });
   }
@@ -190,7 +188,6 @@ export class BitcoinApiService {
     key: ({ address }) => `getAddressTxsUtxo:${address}`,
     ttl: 10_000,
   })
-  @PLimit({ concurrency: 100 })
   public async getAddressTxsUtxo({ address }: { address: string }) {
     return this.call('getAddressTxsUtxo', { address });
   }
@@ -200,7 +197,6 @@ export class BitcoinApiService {
     key: ({ address, afterTxid }) => `getAddressTxs:${address}:${afterTxid}`,
     ttl: 10_000,
   })
-  @PLimit({ concurrency: 100 })
   public async getAddressTxs({ address, afterTxid }: { address: string; afterTxid?: string }) {
     return this.call('getAddressTxs', { address, afterTxid });
   }
@@ -236,7 +232,6 @@ export class BitcoinApiService {
     key: ({ txids }) => `getTransactionTimes:${txids.join(',')}`,
     ttl: 10_000,
   })
-  @PLimit({ concurrency: 100 })
   public async getTransactionTimes({ txids }: { txids: string[] }) {
     return this.call('getTransactionTimes', { txids });
   }
--- backend/src/modules/api.module.ts
+++ backend/src/modules/api.module.ts
@@ -2,7 +2,7 @@ import { join } from 'node:path';
 import { ExecutionContext, Injectable, Module } from '@nestjs/common';
 import { APP_FILTER, APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
-import { GqlExecutionContext, GraphQLModule } from '@nestjs/graphql';
+import { GqlExecutionContext, GraphQLModule, Int } from '@nestjs/graphql';
 import { DataLoaderInterceptor } from 'src/common/dataloader';
 import { Env } from 'src/env';
 import { CkbModule } from './ckb/ckb.module';
@@ -15,8 +15,11 @@ import { ComplexityPlugin } from './complexity.plugin';
 import * as Sentry from '@sentry/nestjs';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { FastifyReply, FastifyRequest } from 'fastify';
-import { SentryGlobalGraphQLFilter } from '@sentry/nestjs/setup';
+import { ApolloServerPluginCacheControl } from '@apollo/server/plugin/cacheControl';
+import responseCachePlugin from '@apollo/server-plugin-response-cache';
 import { AllExceptionsFilter } from 'src/filters/all-exceptions.filter';
+import { DirectiveLocation, GraphQLBoolean, GraphQLDirective, GraphQLEnumType } from 'graphql';
+import { LoggingPlugin } from './logging.plugin';
 
 @Injectable()
 export class GqlThrottlerGuard extends ThrottlerGuard {
@@ -49,9 +52,41 @@ export class GqlThrottlerGuard extends ThrottlerGuard {
         introspection: true,
         graphiql: true,
         autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+        plugins: [
+          ApolloServerPluginCacheControl({
+            defaultMaxAge: 10,
+            calculateHttpHeaders: true,
+          }),
+          responseCachePlugin(),
+        ],
         buildSchemaOptions: {
           dateScalarMode: 'timestamp',
           fieldMiddleware: [fieldPerformanceMiddleware],
+          directives: [
+            new GraphQLDirective({
+              name: 'cacheControl',
+              args: {
+                maxAge: { type: Int },
+                scope: {
+                  type: new GraphQLEnumType({
+                    name: 'CacheControlScope',
+                    values: {
+                      PUBLIC: {},
+                      PRIVATE: {},
+                    },
+                  }),
+                },
+                inheritMaxAge: { type: GraphQLBoolean },
+              },
+              locations: [
+                DirectiveLocation.FIELD_DEFINITION,
+                DirectiveLocation.OBJECT,
+                DirectiveLocation.INTERFACE,
+                DirectiveLocation.UNION,
+                DirectiveLocation.QUERY,
+              ],
+            }),
+          ],
         },
         context: (request: FastifyRequest, reply: FastifyReply) => {
           return {
@@ -84,6 +119,7 @@ export class GqlThrottlerGuard extends ThrottlerGuard {
       useClass: AllExceptionsFilter,
     },
     ComplexityPlugin,
+    LoggingPlugin,
   ],
 })
 export class ApiModule { }
