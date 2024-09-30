44ecc6598d06a05e85d973b9443d267bdda2aa34
ahonn <yuexunjiang@gmail.com>
2024-09-30 11:44:00 +1000

    feat: add latency to healthcheck

--- backend/src/core/ckb-explorer/ckb-explorer.health.ts
+++ backend/src/core/ckb-explorer/ckb-explorer.health.ts
@@ -5,19 +5,18 @@ import * as Sentry from '@sentry/nestjs';
 
 @Injectable()
 export class CkbExplorerHealthIndicator extends HealthIndicator {
-  constructor(
-    private ckbExplorerService: CkbExplorerService,
-
-  ) {
+  constructor(private ckbExplorerService: CkbExplorerService) {
     super();
   }
 
   public async isHealthy(): Promise<HealthIndicatorResult> {
     try {
+      const now = performance.now();
       const stats = await this.ckbExplorerService.getStatistics();
       const isHealthy = !!stats.data.attributes.tip_block_number;
       const result = this.getStatus('ckb-explorer', isHealthy, {
         stats: stats.data.attributes,
+        latency: performance.now() - now,
       });
       if (isHealthy) {
         return result;
